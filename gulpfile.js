03e26d8b6461db7aca0048d21f9ec07aaa268fd4
honminyue <honminyue@gmail.com>
2015-12-01 17:48:27 +0800

    postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,35 @@
 var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var cssnext = require("gulp-cssnext");
+var precss = require('precss');
+var autoprefixer = require('autoprefixer');
 var del = require('del');
 
 var paths = {
-  styles: 'sass/**/*.scss',
+  styles: 'app/sass/style.css',
   scripts: 'app/js/*.js',
   images: 'app/img/**/*'
 };
+
+// Styles Task
+gulp.task('styles', function(){
+  var processors = [
+    autoprefixer({browsers: ['last 2 version', 'ie 8', 'ie 9', 'ie 7']}),
+    precss
+  ];
+  return gulp.src(paths.styles)
+    .pipe(postcss(processors))
+    .pipe(cssnext({
+      sourcemap: true
+    }))
+    .pipe(gulp.dest('dist/css'));
+});
 
 // Scripts Task
 gulp.task('scripts', function(){
   gulp.src(paths.scripts)
     // .pipe()
     .pipe(gulp.dest('dist/js'));
-});
-
-// Styles Task
-gulp.task('styles', function(){
-  gulp.src(paths.styles)
-    // .pipe()
-    .pipe(gulp.dest('dist/css'));
 });
 
 // Images Task
