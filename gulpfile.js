a8d8c27394400358d309762756d2fd2f2a00c0d0
honminyue <honminyue@gmail.com>
2015-12-04 21:00:28 +0800

    BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require('gulp');
-var connect = require('gulp-connect');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
@@ -8,6 +7,7 @@ var postcss = require('gulp-postcss');
 var cssnext = require("gulp-cssnext");
 var precss = require('precss');
 var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync');
 var del = require('del');
 
 var paths = {
@@ -18,18 +18,16 @@ var paths = {
 
 // Styles Task
 gulp.task('styles', function(){
-
   var processors = [
     autoprefixer({browsers: ['last 2 version', 'ie 8', 'ie 9']}),
     precss
   ];
-
   return gulp.src('app/sass/**/styles.css')
     .pipe(postcss(processors))
     .pipe(cssnext({
       sourcemap: true
     }))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('.tmp/css'));
 });
 
 // Scripts Task
@@ -52,16 +50,53 @@ gulp.task('images', function(){
 // Html Task
 gulp.task('html', function () {
   gulp.src('app/*.html')
-    .pipe(connect.reload());
+    // .pipe(connect.reload());
 });
 
-// Connect Task
-gulp.task('connect', function() {
-  connect.server({
-    root: 'app',
-    livereload: true
+// Server Task
+gulp.task('serve', ['styles'], function () {
+  var files = [
+    'app/**/*.html',
+    'app/sass/**/*.css',
+    'app/img/**/*.png',
+    'app/js/**/*.js'
+  ];
+  browserSync.init(files, {
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: 'app'
+    }
   });
+  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.images, ['images']);
+  gulp.watch(['app/*.html'], ['html']);
 });
+
+// gulp.task('serve', ['styles', 'fonts'], () => {
+//   browserSync({
+//     notify: false,
+//     port: 9000,
+//     server: {
+//       baseDir: ['.tmp', 'app'],
+//       routes: {
+//         '/bower_components': 'bower_components'
+//       }
+//     }
+//   });
+
+//   gulp.watch([
+//     'app/*.html',
+//     'app/scripts/**/*.js',
+//     'app/images/**/*',
+//     '.tmp/fonts/**/*'
+//   ]).on('change', reload);
+
+//   gulp.watch('app/styles/**/*.scss', ['styles']);
+//   gulp.watch('app/fonts/**/*', ['fonts']);
+//   gulp.watch('bower.json', ['wiredep', 'fonts']);
+// });
 
 // Watch Task
 gulp.task('watch', function(){
@@ -75,7 +110,7 @@ gulp.task('watch', function(){
 gulp.task('clean', del.bind(null, ['.tmp', 'dist', '.sass-cache']));
 
 // Serve Task
-gulp.task('serve', ['connect', 'watch']);
+// gulp.task('serve', ['browser-sync', 'watch']);
 
 // Default Task
 gulp.task('default', ['styles', 'scripts', 'images']);
